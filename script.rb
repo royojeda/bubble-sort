84b68b733f568993b923f584cba90b1d1186d6c2
Dennis Ojeda <royojeda1@gmail.com>
2022-05-03 02:00:33 +0800

    Optimize logic to stop sorting if already done
    Checks if a swap is made in the last pass. Ends sorting if none.

--- script.rb
+++ script.rb
@@ -2,17 +2,23 @@ def bubble_sort(values)
   pass = 1
   while pass < values.length do
     index = 0
+    flag = 0
     while (index < (values.length - 1 - pass + 1)) do
       if values[index] > values[index + 1]
         temp = values[index + 1]
         values[index + 1] = values[index]
         values[index] = temp
+        flag = 1
       end
       index += 1
     end
+    if flag == 0 && pass < values.length - 1
+      p "Finished early after only #{pass} pass/es out of worst case #{values.length - 1} pass/es"
+      return values 
+    end
     pass += 1
-    p values
   end
+  values
 end
 
-bubble_sort([5,4,3,2,1,0])
+p bubble_sort([4,3,78,2,0,2])
